d5bac2a68444a4358e6a16231e275d04dc940f5e
hmzshin <hamzasahin@outlook.com>
2024-01-16 19:46:54 +0300

    özellik: Yeni bir yatak odası objesi oluşturur.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Wall wall1 = new Wall("north");
+        Wall wall2 = new Wall("eats");
+        Wall wall3 = new Wall("west");
+        Wall wall4 = new Wall("south");
+        Ceiling ceiling= new Ceiling(3,PaintColor.GREEN);
+        Bed bed=new Bed(2,80,2,2);
+        Lamp lamp = new Lamp(LampType.PHARMACY,true,4);
+        Wardrobe wardrobe = new Wardrobe(1,2,30);
+        Carpet carpet = new Carpet(2,2,PaintColor.BROWN);
+
+
+
+
+
+
+        Bedroom bedroom = new Bedroom("bedroom",wall1,wall2,wall3,wall4,ceiling,bed,wardrobe,carpet);
+
+
+        System.out.println(bedroom);
     }
 }
